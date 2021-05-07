fb51cf2773adba3c1557b1a66236a4f647562a3f
Corbaz Alexandre <alexbourlis@gmail.com>
2021-05-07 16:06:31 +0200

    fichiers de bases

---
+++ main.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "ch.h"
+#include "hal.h"
+#include "memory_protection.h"
+#include <usbcfg.h>
+#include <main.h>
+#include <chprintf.h>
+#include <motors.h>
+#include <audio/microphone.h>
+#include <sensors/proximity.h>
+#include <arm_math.h>
+
+
+messagebus_t bus;
+MUTEX_DECL(bus_lock);
+CONDVAR_DECL(bus_condvar);
+
+
+static void serial_start(void)
+{
+	static SerialConfig ser_cfg = {
+	    115200,
+	    0,
+	    0,
+	    0,
+	};
+
+	sdStart(&SD3, &ser_cfg); // UART3.
+}
+
+//static void timer12_start(void){
+//    //General Purpose Timer configuration
+//    //timer 12 is a 16 bit timer so we can measure time
+//    //to about 65ms with a 1Mhz counter
+//    static const GPTConfig gpt12cfg = {
+//        1000000,        /* 1MHz timer clock in order to measure uS.*/
+//        NULL,           /* Timer callback.*/
+//        0,
+//        0
+//    };
+//
+//    gptStart(&GPTD12, &gpt12cfg);
+//    //let the timer count to max value
+//    gptStartContinuous(&GPTD12, 0xFFFF);
+//}
+
+int main(void)
+{
+	halInit();
+	chSysInit();
+
+	 /** Inits the Inter Process Communication bus. */
+	messagebus_init(&bus, &bus_lock, &bus_condvar);
+
+	//starts the serial communication
+	serial_start();
+	//start the USB communication
+	usb_start();
+	//start proximity detector
+	proximity_start();
+
+	palClearPad(GPIOD, GPIOD_LED3);
+
+    /* Infinite loop. */
+    while (1) {
+    	chprintf((BaseSequentialStream *)&SD3, "VALUE = %d \n", get_calibrated_prox(0));
+    	//chThdSleepMilliseconds(1000);
+
+    }
+}
+
+#define STACK_CHK_GUARD 0xe2dee396
+uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
+
+void __stack_chk_fail(void)
+{
+    chSysHalt("Stack smashing detected");
+}
