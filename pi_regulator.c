6d13d9dfa8f26d8eb3dee6d11a1dbab831acbf1d
nassimkhatori <81374913+nassimkhatori@users.noreply.github.com>
2021-05-11 21:56:12 +0200

    Add files via upload

--- pi_regulator.c
+++ pi_regulator.c
@@ -11,6 +11,10 @@
 #include <process_image.h>
 #include "sensors/VL53L0X/VL53L0X.h"
 
+static uint8_t color_now = 0;
+static uint8_t offset_correction = 0;
+
+
 //simple PI regulator implementation
 int16_t pi_regulator(float line_position, float goal){
 
@@ -65,27 +69,92 @@ static THD_FUNCTION(PiRegulator, arg) {
         }
 
 
+        if(get_color_pid()==RED && offset_correction==0)
+        {
+        	color_now=RED;
+        	offset_correction=1;
+        }
+        else if (get_color_pid()==BLACK && offset_correction==0)
+        {
+        	color_now=BLACK;
+        }
+        else if(get_color_pid()==YELLOW && offset_correction==0)
+        {
+        	color_now=YELLOW;
+        }
+        else if(get_color_pid()==BLUE && offset_correction==0)
+        {
+        	color_now=BLUE;
+        	offset_correction=1;
+        }
         //different scenarios in the game board
-		if((get_color_pid()==BLACK || get_color_pid()==YELLOW) && VL53L0X_get_dist_mm()>100)
+		if((color_now==BLACK || color_now==YELLOW)  && VL53L0X_get_dist_mm()>100)
 		{
 			//continue going forward and trying to correct the alignment with the black line
 			right_motor_set_speed(BASESPEED - speed_correction);
 			left_motor_set_speed(BASESPEED + speed_correction);
-		}else if(get_color_pid()==RED && VL53L0X_get_dist_mm()>100)
+		}
+		else if(color_now==RED && VL53L0X_get_dist_mm()>100)
 		{
+			if(offset_correction==1)
+			{
+			right_motor_set_speed(BASESPEED);
+			left_motor_set_speed(BASESPEED);
+			offset_correction=2;
+			chThdSleepUntilWindowed(time, time + MS2ST(900));
+			}
+			else if(offset_correction==2)
+			{
+				right_motor_set_speed(BASESPEED);
+				left_motor_set_speed(-BASESPEED);
+				offset_correction=0;
+				chThdSleepUntilWindowed(time, time + MS2ST(600));
+			}
+
 			//turn left
 			//we need to adjust the values for the speeds to make a left turn that suits the board setup!
-			right_motor_set_speed(BASESPEED);
-			left_motor_set_speed(BASESPEED/2);
+			//right_motor_set_speed(BASESPEED);
+			//left_motor_set_speed(BASESPEED/2);
 			//we need to buy time so we make the thread sleep for X amount of time, needs to be tested
-			chThdSleepUntilWindowed(time, time + MS2ST(300));
-		}else
+		}
+		else if(color_now==BLUE && VL53L0X_get_dist_mm()>100)
+		{
+			if(offset_correction==1)
+			{
+			right_motor_set_speed(BASESPEED);
+			left_motor_set_speed(BASESPEED);
+			offset_correction=2;
+			chThdSleepUntilWindowed(time, time + MS2ST(900));
+			}
+			else if(offset_correction==2)
+			{
+				right_motor_set_speed(-BASESPEED);
+				left_motor_set_speed(BASESPEED);
+				offset_correction=0;
+				chThdSleepUntilWindowed(time, time + MS2ST(600));
+			}
+
+					//turn left
+					//we need to adjust the values for the speeds to make a left turn that suits the board setup!
+					//right_motor_set_speed(BASESPEED);
+					//left_motor_set_speed(BASESPEED/2);
+					//we need to buy time so we make the thread sleep for X amount of time, needs to be tested
+		}
+
+		else
 		{
 			//useless code just for testing
 			right_motor_set_speed(0);
 			left_motor_set_speed(0);
 		}
 
+		/*else if(VL53L0X_get_dist_mm()<100)
+				{
+					right_motor_set_speed(BASESPEED);
+					left_motor_set_speed(-BASESPEED);
+					chThdSleepUntilWindowed(time, time + MS2ST(1300));
+
+				}*/
 		//USEFUL CODE JUST NEED TO GET THE BOARD READY
 		/*else if(get_color_pid()==RED && VL53L0X_get_dist_mm()>100)
 		{
